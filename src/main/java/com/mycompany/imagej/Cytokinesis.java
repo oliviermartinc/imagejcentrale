9fa4e15a63ec9efb0eec461563480792123fa48e
oliviermartinc <olivier.martinc@gmail.com>
2017-11-28 09:14:21 +0100

    feat(plugin): plugin qui marche

--- src/main/java/com/mycompany/imagej/Cytokinesis.java
+++ src/main/java/com/mycompany/imagej/Cytokinesis.java
@@ -8,22 +8,21 @@
 
 package com.mycompany.imagej;
 
-import java.awt.Button;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.Panel;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.*;
 import ij.IJ;
 import ij.plugin.PlugIn;
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.WindowManager;
-import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.plugin.frame.PlugInFrame;
 import ij.plugin.frame.RoiManager;
 import ij.process.ImageProcessor;
+import ij.gui.*;
 
 /**
  * A template for processing each pixel of either
@@ -31,16 +30,31 @@ import ij.process.ImageProcessor;
  *
  * @author Olivier Martin
  */
-public class Cytokinesis extends PlugInFrame implements ij.plugin.PlugIn {
+public class Cytokinesis extends PlugInFrame implements ij.plugin.PlugIn, ActionListener, MouseListener {
 	
 	static Frame Instance;
-	
 	protected ImagePlus image;
 	protected ImagePlus image1;
 	private Panel panel;
+	java.awt.Checkbox AddOnClick;
+	boolean done = false;
+	private ImageCanvas previousCanvas = null;
+	private Roi AutoROI = null;
+	private int position_X = 0;
+	protected int position_Y = 0;
+	protected int slice_1 = 0;
+	protected int slice_2 = 0;
+	protected int slice_3 = 0;
+	public Button button_1 = null;
+	public Button button_2 = null;
+	public Button button_3 = null;
+	public Button button_4 = null;
+	public Button button_5 = null;
+	//private boolean button1_ready = false;
+	//private ij.gui.Roi Roi;
 	
 	public Cytokinesis () {
-		super ("Cytokinesis");
+		super("Cytokinesis");
 		if (Instance != null){
             Instance.toFront();
         }
@@ -48,17 +62,42 @@ public class Cytokinesis extends PlugInFrame implements ij.plugin.PlugIn {
 		Instance = this;
 		//ImagePlus.addImageListener(this);
         WindowManager.addWindow(this);
-        setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
-
+        setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+        
         panel = new Panel();
         panel.setLayout(new GridLayout(10, 0, 0, 0));
+        
+        button_1 = new Button("Start Mitosis");
+        button_1.addActionListener(this);
+        button_1.setEnabled(slice_1 == 0 );
+		panel.add("Start Mitosis",button_1);
+		
+		button_2 = new Button("Start abscission");
+        button_2.addActionListener(this);
+        button_2.setEnabled(slice_1 != 0);
+		panel.add("Start abscission",button_2);
+		
+		button_3 = new Button("End abscission");
+        button_3.addActionListener(this);
+        button_3.setEnabled(slice_1 != 0 && slice_2 != 0);
+		panel.add("End abscission",button_3);
+        
+        Panel p = new Panel();
+        p.setLayout(new BorderLayout());
+        button_4 = new Button("cancel");
+        button_4.addActionListener(this);
+        button_5 = new Button("Add ROI");
+        button_5.addActionListener(this);
+        p.add(button_5, BorderLayout.EAST);
+        p.add(button_4, BorderLayout.WEST);
+        button_5.setEnabled(false);
+        panel.add(p);
+		
+        add(panel);
+        this.setSize(200, 150);
+        this.setVisible(true);
+        getManager();
 		}
-	}
-	
-	void addButton(String label) {
-		Button b = new Button(label);
-		//b.addActionListener(this);
-		panel.add(b);
 	}
 	
 	RoiManager getManager() {
@@ -68,14 +107,119 @@ public class Cytokinesis extends PlugInFrame implements ij.plugin.PlugIn {
         else
             return instance;
     }
-
-	@Override
-	public void run(String string) {
-		IJ.log("test");
-		ImagePlus imp = WindowManager.getCurrentImage();
-		getManager();
+	
+	public void actionPerformed(ActionEvent e) {
+		String label = e.getActionCommand();
+		if (label==null)
+			return;
+		String command = label;
+        		if(command.equals("Start Mitosis"))
+        			startMitosis();
+        		if(command.equals("Start abscission"))
+        			startAbscission();
+        		if(command.equals("End abscission"))
+        			endAbscission();
+        		if(command.equals("cancel"))
+        			cancel();
+        		if(command.equals("Add ROI"))
+        			addRoi();
 	}
-
+	
+	public void startMitosis() {
+		IJ.setTool("point");
+		ImagePlus imp = WindowManager.getCurrentImage();
+		//slice_1 = imp.getCurrentSlice();
+		//IJ.log(Integer.toString(slice_1));
+    }
+	
+	public void startAbscission() {
+		ImagePlus imp = WindowManager.getCurrentImage();
+		slice_2 = imp.getCurrentSlice();
+		button_3.setEnabled(true);
+		button_2.setEnabled(false);
+    }
+	
+	public void endAbscission() {
+		ImagePlus imp = WindowManager.getCurrentImage();
+		slice_3 = imp.getCurrentSlice();
+		button_3.setEnabled(false);
+		button_5.setEnabled(true);
+    }
+	public void cancel() {
+		button_5.setEnabled(false);
+		button_3.setEnabled(false);
+		button_2.setEnabled(false);
+		button_1.setEnabled(true);
+		slice_1 = 0;
+		slice_2 = 0;
+		slice_3 = 0;
+		position_X = 0;
+		position_Y = 0;
+    }
+	public void addRoi() {
+		AutoROI = new Roi(position_X,position_Y,1,1);
+		String position = slice_1 +"_"+slice_2 +"_"+slice_3;
+		AutoROI.setName(position);
+		getManager().addRoi(AutoROI);
+		button_3.setEnabled(false);
+		button_2.setEnabled(false);
+		button_1.setEnabled(true);
+		slice_1 = 0;
+		slice_2 = 0;
+		slice_3 = 0;
+		position_X = 0;
+		position_Y = 0;
+    }
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		ImagePlus imp = WindowManager.getCurrentImage();
+		//IJ.log("tesss");
+		position_X = e.getX();
+		position_Y = e.getY();
+		slice_1 = imp.getCurrentSlice();
+		button_2.setEnabled(true);
+		button_1.setEnabled(false);
+        //IJ.log(Integer.toString(x));
+        //IJ.log(Integer.toString(y));
+        //IJ.log(Integer.toString(imp.getCurrentSlice()));
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {}
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	@Override
+	
+	public void run(String string) {
+		//IJ.log("test");
+		while (!done) {
+			try {Thread.sleep(500);}
+			catch(InterruptedException e) {}
+			ImagePlus imp = WindowManager.getCurrentImage();
+			if (imp != null){
+				ImageCanvas canvas = imp.getCanvas();
+                          	if (canvas != previousCanvas){
+					if(previousCanvas != null)
+                                               previousCanvas.removeMouseListener(this);
+					canvas.addMouseListener(this);
+					previousCanvas = canvas;
+				}
+			}
+                        else{
+                            if(previousCanvas != null)
+                                previousCanvas.removeMouseListener(this);
+                            previousCanvas = null;
+                        }
+                           
+		}
+	}
+	
 	public static void main(String[] args) {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
 		Class<?> clazz = Cytokinesis.class;
@@ -88,7 +232,7 @@ public class Cytokinesis extends PlugInFrame implements ij.plugin.PlugIn {
 		new ImageJ();
 		
 		// open the Clown sample
-		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
+		ImagePlus image = IJ.openImage("/Users/olivier/Downloads/1_LucDMSO-10.tif");
 		image.show();
 		
 		// run the plugin
